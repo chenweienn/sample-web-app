64b8a9ef2eab4d14ad346c30fdc6c742c91f3936
tanzu_gitops_user <tan.zu.ext@siemens.com>
2024-05-20 17:44:40 +0800

    #4 greeting message

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "#3 Hi, Greetings from Spring Boot + Tanzu!";
+		return "#4 Hi, Greetings from Spring Boot + Tanzu!";
 	}
 
 }
