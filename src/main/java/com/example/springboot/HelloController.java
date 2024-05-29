07c1a06aeb721e2c45778c283afc262c36abd4ed
tanzu_gitops_user <tan.zu.ext@siemens.com>
2024-05-29 13:39:43 +0800

    import package to support PathVariable

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -2,6 +2,7 @@ package com.example.springboot;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 public class HelloController {
